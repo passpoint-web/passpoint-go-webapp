1be704061324854e69ae00e777cf3f24c23e8e4d
joshanaba <anabajoshchi@gmail.com>
2023-08-24 14:18:50 +0100

    added toast

--- app/auth-layout.js
+++ app/auth-layout.js
@@ -1,8 +1,14 @@
 import './globals.css'
 import AuthHeader from '@/components/Auth/Header'
 import FullScreenLoader from '@/components/Modal/FullScreenLoader'
+// import {notify} from '@/components/Toast'
 import Head from 'next/head'
+// import { useEffect } from 'react'
 export default function AuthLayout({ children, btn, pageTitle = '', fullScreenLoader }) {
+	// useEffect(()=>{
+	// 	notify('yoyoyoy')
+	// },[])
+	
 	return (
 		<div>
 			<Head>
