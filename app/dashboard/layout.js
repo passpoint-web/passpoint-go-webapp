160f3b147726d3900f5b0f9b56edf8ddaeabb907
omobankole <oluwabankyusuf1@gmail.com>
2023-10-20 11:51:45 +0100

    notification UI

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,18 +1,22 @@
-// 'use client'
-import DashboardHeader from '@/components/Dashboard/Header'
-import DashboardSidebar from '@/components/Dashboard/Sidebar'
-import styles from '@/assets/styles/dashboard-layout.module.css'
+"use client";
+import DashboardHeader from "@/components/Dashboard/Header";
+import DashboardSidebar from "@/components/Dashboard/Sidebar";
+import styles from "@/assets/styles/dashboard-layout.module.css";
+import Notification from "@/components/Notification/Notification";
+import { useState } from "react";
 
 export default function DashboardLayout({ children }) {
-	return (
-		<div className={styles.dashLayout}>
-			<aside>
-				<DashboardSidebar />
-			</aside>
-			<div className={styles.dash_children}>
-				<DashboardHeader />
-				<main className={styles.dash_outlet}>{children}</main>
-			</div>
-		</div>
-	)
+  const [openNotify, setOpenNotify] = useState(false);
+  return (
+    <div className={styles.dashLayout}>
+      <aside>
+        <DashboardSidebar />
+      </aside>
+      <div className={styles.dash_children}>
+        <DashboardHeader setOpenNotify={setOpenNotify} />
+        <main className={styles.dash_outlet}>{children}</main>
+      </div>
+      <Notification setOpenNotify={setOpenNotify} openNotify={openNotify} />
+    </div>
+  );
 }
--- components/Modal/ModalWrapper.js
+++ components/Modal/ModalWrapper.js
@@ -1,63 +1,83 @@
-import styles from './modal.module.css'
-import { CancelIcon } from '@/constants/icons'
+"use client";
+import styles from "./modal.module.css";
+import { CancelIcon } from "@/constants/icons";
 // import PrimaryBtn from '../Btn/Primary'
-import { useRef } from 'react'
-import Button from '../Btn/Button'
-const ModalWrapper = ({children, ctaBtnType = 'md', loading=false, bottomCancelNeeded = true, subHeading, heading, contentStyle, onClose, handleCta, overlayClose = false, ctaBtnText = 'Proceed', cancelBtnText='Cancel'}) => {
-	const modalCtnRef = useRef(null)
-	const modalBgRef = useRef(null)
-	// const modalChildRef = useRef(null)
-	// const closeModal = () => {
-	// 	// Take modals away with transition
-	// 	console.log(modalCtnRef.current.children)
-	// 	// document.querySelectorAll(modalCtn child).forEach((modal) => {
-	// 	// 	modal.classList.add('squeeze-in')
-	// 	// })
-	// 	document
-	// 		.querySelectorAll('modal_container overlay_screen')
-	// 		.forEach((modal) => {
-	// 			modal.classList.add('blur-out')
-	// 		})
-	// 	window.setTimeout(() => {
-	// 		// onClose()
-	// 	}, 300)
-	// }
+import { useRef } from "react";
+import Button from "../Btn/Button";
+const ModalWrapper = ({
+  children,
+  ctaBtnType = "md",
+  loading = false,
+  bottomCancelNeeded = true,
+  subHeading,
+  heading,
+  contentStyle,
+  onClose,
+  handleCta,
+  overlayClose = false,
+  ctaBtnText = "Proceed",
+  cancelBtnText = "Cancel",
+}) => {
+  const modalCtnRef = useRef(null);
+  const modalBgRef = useRef(null);
+  // const modalChildRef = useRef(null)
+  // const closeModal = () => {
+  // 	// Take modals away with transition
+  // 	console.log(modalCtnRef.current.children)
+  // 	// document.querySelectorAll(modalCtn child).forEach((modal) => {
+  // 	// 	modal.classList.add('squeeze-in')
+  // 	// })
+  // 	document
+  // 		.querySelectorAll('modal_container overlay_screen')
+  // 		.forEach((modal) => {
+  // 			modal.classList.add('blur-out')
+  // 		})
+  // 	window.setTimeout(() => {
+  // 		// onClose()
+  // 	}, 300)
+  // }
 
-	return (
-		<div ref={modalCtnRef}
-			className={`${styles.modal_container} reveals`}>
-			<div ref={modalBgRef}
-				className={styles.overlay_screen}
-				onClick={overlayClose ? onClose : null} />
-			<div className={styles.child_ctn}>
-				<button className={`${styles.close_btn} button`}
-					onClick={onClose}>
-					<CancelIcon />
-				</button>
-				<div
-					className={styles.child}
-					style={{...contentStyle}}>
-					<div className={styles.mobile_top}>
-						<div className={styles.dragger} />
-					</div>
-					<div className={styles.top}>
-						<h2>{heading}</h2>
-						<p>{subHeading}</p>
-					</div>
-					{children}
-					<div className={`${styles.bottom} ${bottomCancelNeeded  && styles.end}`}>
-						{bottomCancelNeeded && <Button className="secondary md"
-							onClick={onClose}
-							text={cancelBtnText} />}
-						<Button className={`primary ${ctaBtnType}`}
-							onClick={handleCta}
-							loading={loading}
-							text={ctaBtnText} />
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div ref={modalCtnRef} className={`${styles.modal_container} reveals`}>
+      <div
+        ref={modalBgRef}
+        className={styles.overlay_screen}
+        onClick={overlayClose ? onClose : null}
+      />
+      <div className={styles.child_ctn}>
+        <button className={`${styles.close_btn} button`} onClick={onClose}>
+          <CancelIcon />
+        </button>
+        <div className={styles.child} style={{ ...contentStyle }}>
+          <div className={styles.mobile_top}>
+            <div className={styles.dragger} />
+          </div>
+          <div className={styles.top}>
+            <h2>{heading}</h2>
+            <p>{subHeading}</p>,
+          </div>
+          {children}
+          <div
+            className={`${styles.bottom} ${bottomCancelNeeded && styles.end}`}
+          >
+            {bottomCancelNeeded && (
+              <Button
+                className="secondary md"
+                onClick={onClose}
+                text={cancelBtnText}
+              />
+            )}
+            <Button
+              className={`primary ${ctaBtnType}`}
+              onClick={handleCta}
+              loading={loading}
+              text={ctaBtnText}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ModalWrapper
+export default ModalWrapper;
