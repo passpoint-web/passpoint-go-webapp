b491208510039b88661f912fa3bc238f70c2591e
omobankole <oluwabankyusuf1@gmail.com>
2023-10-24 19:13:21 +0100

    wallet overview

--- app/dashboard/wallet/page.js
+++ app/dashboard/wallet/page.js
@@ -2,9 +2,11 @@ import Wallet from "@/components/Wallet";
 import React from "react";
 
 const WalletPage = () => {
-  return <>
-    <Wallet />
-  </>;
+  return (
+    <>
+      <Wallet />
+    </>
+  );
 };
 
 export default WalletPage;
