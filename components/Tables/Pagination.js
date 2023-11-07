970e9ae9466110edce89d2f653ae102a1839dc72
joshanaba <anabajoshchi@gmail.com>
2023-11-07 22:39:38 +0100

    .

--- components/Tables/Pagination.js
+++ components/Tables/Pagination.js
@@ -2,7 +2,7 @@ import PrimaryBtn from "../Btn/Primary"
 
 const Pagination = ({ tableStyles, pagination, handlePaginationEvent }) => {
 	return (
-		<div className={`${tableStyles.table__pagination} ${tableStyles.wallet}`}>
+		<div className={`${tableStyles.table__pagination}`}>
 			<div className={StyleSheet.lhs}>Showing {pagination.pageSize} items</div>
 			<div className={StyleSheet.mhs}>
         Page {pagination.currentPage} of {pagination.totalPages}
