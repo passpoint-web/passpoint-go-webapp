4f55470518de4b511758054eecd044b28a9f65f4
felixfavour <felixfavour0@gmail.com>
2023-11-25 23:17:28 +0100

    chore: updated copy in flight passengers

--- components/Travel/FlightPassengers.js
+++ components/Travel/FlightPassengers.js
@@ -196,7 +196,7 @@ const FlightPassengers = ({
             onClick={() => notify("info", "Update Passenger details here")}
           >
             <PlusIcon />
-            Add Another Passenger
+            Edit Flight Details
           </Link>
         </div>
         <div className={styles.rhs}>
