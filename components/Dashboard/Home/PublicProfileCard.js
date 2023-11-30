967c31a1f787932960cd4767c788b765f6684807
felixfavour <felixfavour0@gmail.com>
2023-11-30 09:06:47 +0100

    chore: began tailwind setup

--- components/Dashboard/Home/PublicProfileCard.js
+++ components/Dashboard/Home/PublicProfileCard.js
@@ -1,25 +1,21 @@
 // import Link from 'next/link'
-import styles from "@/assets/styles/dashboard-layout.module.css";
-import PrimaryLink from "@/components/Link/Primary";
+import styles from "@/assets/styles/dashboard-layout.module.css"
+import PrimaryLink from "@/components/Link/Primary"
 
 const PublicProfileCard = () => {
   return (
     <div className={styles.dashPublic}>
       <div className={styles.dashPublic_content}>
-        <h3>Explore Travel Services</h3>
+        <h3 className="text-xl text-uppercase">Explore Travel Services</h3>
         <p>
           You can book your flights and hotels here seamlessly, What are you
           waiting for?
         </p>
         {/* <Link href="">Start Setup</Link> */}
-        <PrimaryLink
-          type="medium"
-          text="Book Now"
-          href="/travel/flights"
-        />
+        <PrimaryLink type="medium" text="Book Now" href="/travel/flights" />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PublicProfileCard;
+export default PublicProfileCard
