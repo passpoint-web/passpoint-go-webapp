134f6b238a4b2eb720df9cd71036c372bf8cf3bd
felixfavour <felixfavour0@gmail.com>
2023-12-04 18:12:51 +0100

    fix: center icon on 404

--- app/not-found.js
+++ app/not-found.js
@@ -1,9 +1,8 @@
-
-import DashboardHeader from "@/components/Dashboard/Header";
-import styles from "@/assets/styles/dashboard-layout.module.css";
-import Error from "../assets/images/dashboard/error.svg";
-import Image from "next/image";
-import PrimaryLink from "@/components/Link/Primary";
+import DashboardHeader from "@/components/Dashboard/Header"
+import styles from "@/assets/styles/dashboard-layout.module.css"
+import Error from "../assets/images/dashboard/error.svg"
+import Image from "next/image"
+import PrimaryLink from "@/components/Link/Primary"
 
 const NotFound = () => {
   return (
@@ -11,7 +10,9 @@ const NotFound = () => {
       <DashboardHeader styles={styles} />
       <section className={styles.errorPage_main}>
         <div className={styles.errorPage_content}>
-          <Image src={Error} alt="error" />
+          <div className="flex justify-center mb-8">
+            <Image src={Error} alt="error" />
+          </div>
           <h1>{`Oops! You've Hit a Roadblock`}</h1>
           <p>
             {`Looks like you took a wrong turn or the page you're looking for is
@@ -23,7 +24,7 @@ const NotFound = () => {
         </div>
       </section>
     </main>
-  );
-};
+  )
+}
 
-export default NotFound;
+export default NotFound
